4f3b153436621a8d91f2a181dd5d167e87dfbb5d
dalissongabriel <alissongabrieldino@gmail.com>
2020-12-30 16:50:19 -0300

    feat: titulo com styled components

--- src/Components/Titulo/index.jsx
+++ src/Components/Titulo/index.jsx
@@ -1,6 +1,9 @@
-import React from "react";
 
-const Titulo = ({ children }) => {
-  return <h1 className="titulo">{children}</h1>;
-};
+import styled from "styled-components";
+
+const Titulo = styled.h1`
+  color: gray;
+  padding: 25px 0px;
+`;
+
 export default Titulo;
